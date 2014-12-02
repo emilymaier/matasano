d7bd70d57bd0bff1d070ec062ef7777b4e287ad0
Emily Maier <emily@emilymaier.net>
2014-12-01 23:52:36 -0500

    Set 1, Challenge 2

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void hex_to_bytes(unsigned char* str) {
 	size_t idx;
@@ -9,17 +10,41 @@ void hex_to_bytes(unsigned char* str) {
 			str[idx] -= 87;
 		}
 	}
+	size_t size = idx;
 
-	for(idx = 0; str[idx * 2] != '\0'; idx++) {
+	for(idx = 0; idx < size * 2; idx++) {
 		str[idx] = str[idx * 2] << 4;
 		str[idx] += str[idx * 2 + 1];
 	}
 	str[idx] = '\0';
 }
 
-int main() {
+void bytes_to_hex(unsigned char* str, size_t size) {
+	size_t idx = size;
+	str[idx * 2] = '\0';
+	idx--;
+	while(1) {
+		str[idx * 2 + 1] = str[idx] & 15;
+		str[idx * 2] = str[idx] >> 4;
+		if(idx == 0) {
+			break;
+		}
+		idx--;
+	}
+
+	for(idx = 0; idx < size * 2; idx++) {
+		if(str[idx] < 10) {
+			str[idx] += '0';
+		} else {
+			str[idx] += 'a' - 10;
+		}
+	}
+}
+
+void s1c1() {
+	FILE* infile = fopen("inputs1c1.txt", "r");
 	unsigned char input[256];
-	scanf("%255s\n", input);
+	fscanf(infile, "%255s\n", input);
 	hex_to_bytes(input);
 	unsigned char output[256];
 
@@ -71,5 +96,29 @@ int main() {
 	}
 
 	printf("%s\n", output);
+}
+
+void s1c2() {
+	FILE* infile = fopen("inputs1c2.txt", "r");
+	unsigned char input1[256];
+	fscanf(infile, "%255s\n", input1);
+	size_t original_size = strlen((const char*) input1);
+	hex_to_bytes(input1);
+	unsigned char input2[256];
+	fscanf(infile, "%255s\n", input2);
+	hex_to_bytes(input2);
+	unsigned char output[256];
+
+	for(size_t idx = 0; idx < original_size / 2; idx++) {
+		output[idx] = input1[idx] ^ input2[idx];
+	}
+
+	bytes_to_hex(output, original_size / 2);
+	printf("%s\n", output);
+}
+
+int main() {
+	s1c1();
+	s1c2();
 	return 0;
 }
